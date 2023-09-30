b6414bff29faf785bac505028c2c544ae0ccce6e
Tamojay Dey <tamojay@interpl.ai>
2023-09-30 15:08:16 +0530

    force push

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,6 +1,21 @@
 import "dotenv/config";
 import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import bodyParser from "body-parser";
+import compression from "compression";
+
 const app = express();
+
+app.use(
+  cors({
+    credentials: true,
+  })
+);
+
+app.use(compression());
+app.use(cookieParser());
+app.use(bodyParser.json());
 
 app.get("/", (req, res) => {
   res.send("Hello Node and TS!");
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,14 +1,17 @@
 import app from "./app";
 import env from "./util/validateEnv";
 import mongoose from "mongoose";
+import http from "http";
 
 const { PORT, MONGO_URL } = env;
+
+const server = http.createServer(app);
 
 mongoose
   .connect(MONGO_URL!)
   .then(() => {
     console.log("Connection successful!!");
-    app.listen(PORT, () => {
+    server.listen(PORT, () => {
       console.log(`Notes app server listening on port: ${PORT}`);
     });
   })
